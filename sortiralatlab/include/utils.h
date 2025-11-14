e96e461c33c790db4bc8fb75a6e1bfc61741449a
heiko404 <fathir5@mhs.usk.ac.id>
2025-11-14 21:58:43 +0700

    fixed utils.h

--- sortiralatlab/include/utils.h
+++ sortiralatlab/include/utils.h
@@ -19,3 +19,13 @@ int login(char *username, char *password);
 void load_alat(alat_lab *arr, int *n, char *nama_file);
 void menu_admin();
 void view_list();
+void view_list_pinjam();
+void edit_list();
+void add_alat();
+void delete_alat();
+void update_alat();
+void menu_user();
+void pinjam_alat();
+void kembalikan_alat();
+
+#endif
