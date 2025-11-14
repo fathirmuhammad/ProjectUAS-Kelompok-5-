18b5ae4c8ffbf73bdb5828d59bc8d99742c7f817
SITI <sititara@mhs.usk.ac.id>
2025-11-14 17:24:12 +0700

    add variabel judul

---
+++ lagu/lirik.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_WORDS 1000
+#define MAX_WORD_LEN 50
+
+char kataunik[MAX_WORDS][MAX_WORD_LEN]; 
+int hitungkata = 0; 
+char judul[1024] = "";
